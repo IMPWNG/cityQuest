a321affe1fe83e8bd1f9fd8db50045986b682a0e
IMPWNG <matisse.ach+github@gmail.com>
2023-04-25 17:17:43 -0400

    auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../../prisma/client";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+};
+export default NextAuth;
